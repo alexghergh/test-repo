a381af6370d94458c7a0ce2614b8b0e795101511
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-18 14:59:05 +0300

    window

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -1,1 +1,10 @@
-cmake --version
+if ($Env:CONFIG -eq "MSVC") {
+    # Microsoft Visual C Compiler
+    $Env:CC = cl
+    cmake -B build/
+
+} else if ($Env:CONFIG -eq "MINGW") {
+    # MinGW gcc for Windows
+    $Env:CC = gcc
+    cmake -B build/
+}
