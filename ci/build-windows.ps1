6358f4ca3da2fb28b8e466d48c8f862ec3b2f8cc
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-18 17:34:19 +0300

    wind

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -1,10 +1,12 @@
 if ($Env:CONFIG -eq 'MSVC') {
     # Microsoft Visual C Compiler
-    $Env:CC = 'cl'
-    cmake -B build/
+    # $Env:CC = 'cl'
+    # cmake -B build/
 
 } elseif ($Env:CONFIG -eq 'MINGW') {
     # MinGW gcc for Windows
-    $Env:CC = gcc
-    cmake -B build/
+    # $Env:CC = gcc
+    # cmake -B build/
 }
+
+vswhere.exe
