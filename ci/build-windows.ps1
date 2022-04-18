f0f92ba457455f219c491bc50a868730d12612dd
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-19 01:47:57 +0300

    error 2

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -1,10 +1,12 @@
 if ($Env:CONFIG -eq 'MSVC') {
     # Microsoft Visual C Compiler
     $Env:CC = 'cl'
-    cmake -B build/
+    cmake -B build\
+    nmake /F build\Makefile
 
 } elseif ($Env:CONFIG -eq 'MINGW') {
     # MinGW gcc for Windows
     $Env:CC = 'gcc.exe'
-    cmake -G "MinGW Makefiles" -B build/
+    # see https://www.msys2.org/docs/cmake/ for generators
+    cmake -G "MinGW Makefiles" -B build\
 }
--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,9 @@ build_wiregrid() {
     echo "Running cmake.."
     cmake -B build/ .
 
+    echo "Running make.."
+    make -C build/
+
 }
 
 build_wiregrid
