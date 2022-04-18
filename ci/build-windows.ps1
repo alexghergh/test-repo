d8b3a2e7afef58db53dc97cd747f81778708af44
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-18 17:09:35 +0300

    windows

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -1,9 +1,9 @@
-if ($Env:CONFIG -eq "MSVC") {
+if ($Env:CONFIG -eq 'MSVC') {
     # Microsoft Visual C Compiler
-    $Env:CC = cl
+    $Env:CC = 'cl'
     cmake -B build/
 
-} elseif ($Env:CONFIG -eq "MINGW") {
+} elseif ($Env:CONFIG -eq 'MINGW') {
     # MinGW gcc for Windows
     $Env:CC = gcc
     cmake -B build/
