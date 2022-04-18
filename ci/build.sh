cf682ee64bc752e897e0f3cdf779531181ea3525
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-18 12:46:05 +0300

    yeap

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,7 @@
 build_wiregrid() {
 
-    echo "Creating build directory at \"./build/\".."
-    mkdir -p build/
-
     echo "Running cmake.."
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=$CC -B build/ .
+    cmake -B build/ .
 
 }
 
