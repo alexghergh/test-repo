63f477ab813a7c8de248240f3758695e42777d66
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-19 02:24:14 +0300

    error 3

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -2,11 +2,12 @@ if ($Env:CONFIG -eq 'MSVC') {
     # Microsoft Visual C Compiler
     $Env:CC = 'cl'
     cmake -B build\
-    nmake /F build\Makefile
+    cd build\ && nmake /F Makefile
 
 } elseif ($Env:CONFIG -eq 'MINGW') {
     # MinGW gcc for Windows
     $Env:CC = 'gcc.exe'
     # see https://www.msys2.org/docs/cmake/ for generators
     cmake -G "MinGW Makefiles" -B build\
+    make -C build\
 }
