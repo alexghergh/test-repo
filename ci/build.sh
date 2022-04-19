7877392622a3c3ac9ba0a9922252ad87f11ba54b
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-19 17:51:07 +0300

    maybe

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 build_wiregrid() {
 
-    if [ ${OS_NAME} = 'macos-latest' && ${CC} = 'gcc' ]; then
+    if [ ${OS_NAME} = 'macos-latest' ] && [ ${CC} = 'gcc' ]; then
         # since the system's gcc is just a symlink to clang on macos,
         # we have to specifically mention the path to it
         CC='/usr/local/bin/gcc'
