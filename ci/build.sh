bbf9803ff35f23c0fb32d564d822728a2de78080
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-04-19 17:47:32 +0300

    nop

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,15 @@
 build_wiregrid() {
 
-    echo "Running cmake.."
+    if [ ${OS_NAME} = 'macos-latest' && ${CC} = 'gcc' ]; then
+        # since the system's gcc is just a symlink to clang on macos,
+        # we have to specifically mention the path to it
+        CC='/usr/local/bin/gcc'
+    fi
+
+    echo 'Running cmake..'
     cmake -B build/ .
 
-    echo "Running make.."
+    echo 'Running make..'
     make -C build/
 
 }
