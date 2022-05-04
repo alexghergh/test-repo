0e5cf63ef0001e8d2e08970aebc19900d36e768d
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-05-05 02:17:29 +0300

    WORKED

--- src/main.c
+++ src/main.c
@@ -3,5 +3,14 @@
 int main() {
     printf("hey from build\n");
 
+    int x;
+
+    for (int i = 0; i < 2; i++) {
+    if (x == 2)
+        continue;
+    else
+        x++;
+    }
+
     return 0;
 }
